5c772e31cdb5ba5619e5f1a910b192e797064e00
apoorvanand <apoorvanand.2189@gmail.com>
2017-02-01 19:41:38 -0500

    HashTable for novice

---
+++ src/dataStructureImplementations/HashTable.java
@@ -0,0 +1,20 @@
+package dataStructureImplementations;
+
+public class HashTable {
+	String [] array;
+	
+	public HashTable(int n){
+		array = new String[n];
+	}
+
+	public void hashFunction(String[] arr){
+		int size =arr.length;
+		
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
